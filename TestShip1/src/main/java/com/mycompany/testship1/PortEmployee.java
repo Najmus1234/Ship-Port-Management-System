7b37245e834fd5d71a61b0b7c72845cf09b6189d
nafis <nafis@NIHALTWPC>
2025-08-11 18:14:49 +0600

    A class for port employees

--- TestShip1/src/main/java/com/mycompany/testship1/PortEmployee.java
+++ TestShip1/src/main/java/com/mycompany/testship1/PortEmployee.java
@@ -8,6 +8,29 @@ package com.mycompany.testship1;
  *
  * @author 603.16.IP207
  */
-public class PortEmployee {
+public class PortEmployee extends Person{
+   private String shift, workPlace;
+
+    public PortEmployee(String shift, String workPlace, String name, String id, String role, int age, double salary) {
+        super(name, id, role, age, salary);
+        this.shift = shift;
+        this.workPlace = workPlace;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public String getWorkPlace() {
+        return workPlace;
+    }
+
+    public void setWorkPlace(String workPlace) {
+        this.workPlace = workPlace;
+    }
    
 }
