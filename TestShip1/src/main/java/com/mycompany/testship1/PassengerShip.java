1291327b40ce136422d4f27a5db7f90f58066d05
nafis <nafis@NIHALTWPC>
2025-08-11 19:35:02 +0600

    A class for passenger ships

--- TestShip1/src/main/java/com/mycompany/testship1/PassengerShip.java
+++ TestShip1/src/main/java/com/mycompany/testship1/PassengerShip.java
@@ -9,5 +9,68 @@ package com.mycompany.testship1;
  * @author nafis
  */
 public class PassengerShip extends Ship{
-    private int 
+    private int passengerCapacity, passengerCount, lifeboatsCapacity, lifeboatsCount;   
+    private int numberOfCabins;           
+    private boolean hasLifeboats; 
+
+    public PassengerShip(int passengerCapacity, int passengerCount, int lifeboatsCapacity, int lifeboatsCount, int numberOfCabins, boolean hasLifeboats, String shipName, String shipCompany, String registrationID, String captain, String country, String shipStatus, String dockAssigned, int totalCrew, double fuelCapacity, double fuelRemained, double weightCapacity, double currentWeight) {
+        super(shipName, shipCompany, registrationID, captain, country, shipStatus, dockAssigned, totalCrew, fuelCapacity, fuelRemained, weightCapacity, currentWeight);
+        this.passengerCapacity = passengerCapacity;
+        this.passengerCount = passengerCount;
+        this.lifeboatsCapacity = lifeboatsCapacity;
+        this.lifeboatsCount = lifeboatsCount;
+        this.numberOfCabins = numberOfCabins;
+        this.hasLifeboats = hasLifeboats;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public int getPassengerCount() {
+        return passengerCount;
+    }
+
+    public void setPassengerCount(int passengerCount) {
+        this.passengerCount = passengerCount;
+    }
+
+    public int getLifeboatsCapacity() {
+        return lifeboatsCapacity;
+    }
+
+    public void setLifeboatsCapacity(int lifeboatsCapacity) {
+        this.lifeboatsCapacity = lifeboatsCapacity;
+    }
+
+    public int getLifeboatsCount() {
+        return lifeboatsCount;
+    }
+
+    public void setLifeboatsCount(int lifeboatsCount) {
+        this.lifeboatsCount = lifeboatsCount;
+    }
+
+    public int getNumberOfCabins() {
+        return numberOfCabins;
+    }
+
+    public void setNumberOfCabins(int numberOfCabins) {
+        this.numberOfCabins = numberOfCabins;
+    }
+
+    public boolean isHasLifeboats() {
+        return hasLifeboats;
+    }
+
+    public void setHasLifeboats(boolean hasLifeboats) {
+        this.hasLifeboats = hasLifeboats;
+    }
+    
+    
+  
 }
